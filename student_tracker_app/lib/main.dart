059463421e698d4bba59ab89cc5cd6845e27c8b9
Vijayashree A <avijayashree06@gmail.com>
2026-03-10 12:12:06 +0530

    Local UI State

--- student_tracker_app/lib/main.dart
+++ student_tracker_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/scrollable_views.dart';
+import 'screens/state_management_demo.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ScrollableViews(),
+      home: StateManagementDemo(),
     );
   }
 }
